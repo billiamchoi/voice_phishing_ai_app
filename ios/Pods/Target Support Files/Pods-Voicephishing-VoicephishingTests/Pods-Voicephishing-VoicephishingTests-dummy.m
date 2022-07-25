673e30c8a8d76e9785f51d97656334adf188a8aa
billiamchoi <billiamchoi4u@gmail.com>
2022-07-25 15:41:50 +0900

    Apply .gitignore

--- ios/Pods/Target Support Files/Pods-Voicephishing-VoicephishingTests/Pods-Voicephishing-VoicephishingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Voicephishing_VoicephishingTests : NSObject
-@end
-@implementation PodsDummy_Pods_Voicephishing_VoicephishingTests
-@end
--- ios/Pods/Target Support Files/Pods-Voicephishing/Pods-Voicephishing-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Voicephishing : NSObject
-@end
-@implementation PodsDummy_Pods_Voicephishing
-@end
