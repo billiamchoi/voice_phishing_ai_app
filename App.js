ea5239703331ea529b272f8acdd0497ca29af39d
billiamchoi <billiamchoi4u@gmail.com>
2022-08-02 21:34:31 +0900

    react-native voice 예제 추가

--- App.js
+++ App.js
@@ -1,112 +1,213 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from "react"
+import { StyleSheet, Text, View, Image, TouchableHighlight } from "react-native"
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Voice from "@react-native-voice/voice"
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  state = {
+    recognized: "",
+    pitch: "",
+    error: "",
+    end: "",
+    started: "",
+    results: [],
+    partialResults: []
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  constructor(props) {
+    super(props)
+    Voice.onSpeechStart = this.onSpeechStart
+    Voice.onSpeechRecognized = this.onSpeechRecognized
+    Voice.onSpeechEnd = this.onSpeechEnd
+    Voice.onSpeechError = this.onSpeechError
+    Voice.onSpeechResults = this.onSpeechResults
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners)
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  onSpeechStart = e => {
+    console.log("onSpeechStart: ", e)
+    this.setState({
+      started: "√"
+    })
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  onSpeechRecognized = e => {
+    console.log("onSpeechRecognized: ", e)
+    this.setState({
+      recognized: "√"
+    })
+  }
+
+  onSpeechEnd = e => {
+    console.log("onSpeechEnd: ", e)
+    this.setState({
+      end: "√"
+    })
+  }
+
+  onSpeechError = e => {
+    console.log("onSpeechError: ", e)
+    this.setState({
+      error: JSON.stringify(e.error)
+    })
+  }
+
+  onSpeechResults = e => {
+    console.log("onSpeechResults: ", e)
+    this.setState({
+      results: e.value
+    })
+  }
+
+  onSpeechPartialResults = e => {
+    console.log("onSpeechPartialResults: ", e)
+    this.setState({
+      partialResults: e.value
+    })
+  }
+
+  onSpeechVolumeChanged = e => {
+    console.log("onSpeechVolumeChanged: ", e)
+    this.setState({
+      pitch: e.value
+    })
+  }
+
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: "",
+      pitch: "",
+      error: "",
+      started: "",
+      results: [],
+      partialResults: [],
+      end: ""
+    })
+
+    try {
+      await Voice.start("en-US")
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy()
+    } catch (e) {
+      console.error(e)
+    }
+    this.setState({
+      recognized: "",
+      pitch: "",
+      error: "",
+      started: "",
+      results: [],
+      partialResults: [],
+      end: ""
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native Voice!</Text>
+        <Text style={styles.instructions}>
+          Press the button and start speaking.
+        </Text>
+        <Text style={styles.stat}>{`Started: ${this.state.started}`}</Text>
+        <Text
+          style={styles.stat}
+        >{`Recognized: ${this.state.recognized}`}</Text>
+        <Text style={styles.stat}>{`Pitch: ${this.state.pitch}`}</Text>
+        <Text style={styles.stat}>{`Error: ${this.state.error}`}</Text>
+        <Text style={styles.stat}>Results</Text>
+        {this.state.results.map((result, index) => {
+          return (
+            <Text key={`result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          )
+        })}
+        <Text style={styles.stat}>Partial Results</Text>
+        {this.state.partialResults.map((result, index) => {
+          return (
+            <Text key={`partial-result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          )
+        })}
+        <Text style={styles.stat}>{`End: ${this.state.end}`}</Text>
+        <TouchableHighlight onPress={this._startRecognizing}>
+          <Image style={styles.button} source={require("./button.png")} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._stopRecognizing}>
+          <Text style={styles.action}>Stop Recognizing</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._cancelRecognizing}>
+          <Text style={styles.action}>Cancel</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._destroyRecognizer}>
+          <Text style={styles.action}>Destroy</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  button: {
+    width: 50,
+    height: 50
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
   },
-  highlight: {
-    fontWeight: '700',
+  action: {
+    textAlign: "center",
+    color: "#0000FF",
+    marginVertical: 5,
+    fontWeight: "bold"
   },
-});
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  },
+  stat: {
+    textAlign: "center",
+    color: "#B0171F",
+    marginBottom: 1
+  }
+})
 
-export default App;
+export default App
