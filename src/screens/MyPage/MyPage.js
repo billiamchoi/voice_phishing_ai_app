f3bd8babded2326db0e38808738b363933f9a37a
billiamchoi <billiamchoi4u@gmail.com>
2022-09-12 14:07:17 +0900

    rename Asserts to asserts & Mypage work saving

--- src/screens/MyPage/MyPage.js
+++ src/screens/MyPage/MyPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react"
-import { StyleSheet, Text, View, Button } from "react-native"
-
+import { StyleSheet, Text, View } from "react-native"
+import { Avatar } from 'react-native-elements';
+import { Colors } from '../../styles'
 class Mypage extends Component {
   
   render() {
     return (
       <View style={styles.container}>
-        <Text>hi</Text>
+         <Avatar
+            size="xlarge"
+            rounded
+            source={require('../../assets/Images/male_profile.png')}
+            containerStyle={styles.avatarContainer}
+					/>
       </View>
     );
   }
@@ -15,7 +21,12 @@ class Mypage extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Colors.BackgroundColor
   },
+  avatarContainer : {
+    alignSelf: "center", 
+    marginTop: "25%"
+  }
 
 });
 
